e0dca0e753277bb098bd5a76747caa7bc0bda25f
Emilia Szafarowska <56365077+Ristie@users.noreply.github.com>
2021-01-10 20:12:48 +0100

    Nowa wersja projektu
    Poprzednia wersja polegała głównie na CSS, co mocno ograniczało możliwości - ta używa Vuetify

---
+++ Projekt Gridsome - Update/src/pages/About.vue
@@ -0,0 +1,132 @@
+<template>
+<v-container>
+    <v-row>
+      <v-col>
+        <section id="o-mnie">
+    <div class="jumbotron text-center">
+  <p class="display-4">O mnie</p>
+  <p class="lead">Grafik i fotograf amator, entuzjastka psychologii, diagnosta własnego samochodu i licznych komputerów, kolekcjonerka. Fanka gier RPG.</p>
+  <p class="lead">Przyszła inżynier informatyki.</p>
+  <hr class="my-4">
+         </div> 
+</section> 
+      </v-col>
+    </v-row>
+
+    <v-row justify="space-around" class="mb-lg-8">
+      <v-card width="700" color="transparent">
+            <v-spacer></v-spacer>
+
+          <v-card-title class="">
+            <p class="ml-8">
+              Edukacja
+            </p>
+          </v-card-title>
+
+        <v-card-text>
+          <div class="font-weight-bold ml-8 mb-2">
+            Dziś
+          </div>
+
+          <v-timeline
+            align-top
+            dense
+          >
+            <v-timeline-item
+              v-for="message in messages"
+              :key="message.time"
+              :color="message.color"
+              small
+            >
+              <div>
+                <div class="font-weight-normal">
+                  <strong>{{ message.from }}</strong>, {{message.time}}
+                </div>
+                <div>{{ message.message }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+
+      <v-card width="700" color="transparent">
+            <v-spacer></v-spacer>
+
+          <v-card-title class="">
+            <p class="ml-8">
+              Kompetencje
+            </p>
+          </v-card-title>
+
+        <v-card-text>
+          <div class="font-weight-bold ml-8 mb-2">
+          </div>
+
+          <v-timeline
+            align-top
+            dense
+          >
+            <v-timeline-item
+              v-for="ability in abilities"
+              :key="ability.time"
+              :color="ability.color"
+              small
+            >
+              <div>
+                <div class="font-weight-normal">
+                  <strong>{{ ability.description }}</strong>
+                </div>
+                <div>{{ ability.info }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+    </v-row>
+    <section>
+    <div class="jumbotron text-center">
+  <hr class="my-4">
+         </div> 
+</section> 
+
+  </v-container>
+</template>
+
+<script>
+export default {
+      data: () => ({
+      messages: [
+        {
+          from: 'Państwowa Wyższa Szkoła Zawodowa im. Witelona w Legnicy',
+          message: 'Informatyka (Grafika komputerowa)',
+          time: '10.2018 - obecnie',
+          color: 'deep-purple',
+        },
+        {
+          from: 'I Liceum Ogólnokształcące im. ks. Bolka I w Jaworze',
+          message: 'Profil matematyczno-fizyczny',
+          time: '09.2015 - 04.2018',
+          color: 'blue',
+        },
+      ],
+      abilities: [
+        {
+          description:'Znajomość języka angielskiego (ogólna)',
+          info:'Poziom: C1',
+          color:'red darken-1',
+        },
+        {
+          description:'Znajomość języka angielskiego (w biznesie)',
+          info:'Poziom: B2',
+          color:'red',
+        },
+        {
+          description:'Znajomość programu Adobe Photoshop',
+          info:'Poziom: Średnio zaawansowany',
+          color:'orange darken-1',
+        }
+      ]
+    }),
+
+}
+</script>
---
+++ Projekt Gridsome - Update/src/pages/Photos.vue
@@ -0,0 +1,101 @@
+<template>
+
+<section id="fotografie">
+  <v-container>
+    <v-row>
+      <v-col>
+        <div class="jumbotron text-center">
+          <p class="display-3">Fotografie</p>
+        </div>
+      </v-col>
+    </v-row>
+      <v-row class="justify-center">
+        <v-card
+        v-for="photo in photos"
+    :key="photo.name"
+    class="mx-lg-4 my-lg-4"
+    max-width="400"
+    max-height="400"
+  >
+    <v-img
+    height="400px"
+      :src="photo.link"
+    ></v-img>
+  </v-card>
+  </v-row>
+  </v-container>
+</section>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      photos: [
+        {
+          name: '1',
+          link: 'https://raw.githubusercontent.com/Ristie/sm-s3gk/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/26.jpg'
+        },
+        {
+          name: '2',
+          link: 'https://raw.githubusercontent.com/Ristie/sm-s3gk/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20180807_152011.jpg'
+        },
+        {
+          name:'3',
+          link:'https://raw.githubusercontent.com/Ristie/sm-s3gk/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200430_202945.jpg'
+        },
+        {
+          name: '4',
+          link:'https://raw.githubusercontent.com/Ristie/sm-s3gk/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200606_203601.jpg'
+        },
+        {
+          name: '5',
+          link: 'https://raw.githubusercontent.com/Ristie/sm-s3gk/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200607_133741.jpg'
+        },
+        {
+          name: '6',
+          link: 'https://raw.githubusercontent.com/Ristie/sm-s3gk/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200725_162749.jpg'
+        },
+        {
+          name: '7',
+          link: 'https://raw.githubusercontent.com/Ristie/sm-s3gk/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200730_202705.jpg'
+        },
+        {
+          name: '8',
+          link: 'https://github.com/Ristie/sm-s3gk/blob/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200530_211440.png?raw=true'
+        },
+        {
+          name: '9',
+          link: 'https://github.com/Ristie/sm-s3gk/blob/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200601_135633.jpg?raw=true'
+        },
+        {
+          name: '10',
+          link: 'https://github.com/Ristie/sm-s3gk/blob/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200607_133741.jpg?raw=true'
+        },
+        {
+          name: '11',
+          link: 'https://github.com/Ristie/sm-s3gk/blob/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200622_141449.jpg?raw=true'
+        },
+        {
+          name: '12',
+          link: 'https://github.com/Ristie/sm-s3gk/blob/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200704_173301.jpg?raw=true'
+        },
+        {
+          name: '13',
+          link: 'https://github.com/Ristie/sm-s3gk/blob/d9a0a6399d207280efcbeaaad368b9466cfcbad8/Zdj%C4%99cia/IMG_20200711_162003.jpg?raw=true'
+        },
+        {
+          name: '14',
+          link: 'https://github.com/Ristie/sm-s3gk/blob/master/Zdj%C4%99cia/IMG_20200829_183458.jpg?raw=true'
+        },
+        {
+          name: '15',
+          link: 'https://github.com/Ristie/sm-s3gk/blob/master/Zdj%C4%99cia/IMG_20201125_130420.jpg?raw=true'
+        }
+         ]
+    }
+  },
+  
+}
+</script>
